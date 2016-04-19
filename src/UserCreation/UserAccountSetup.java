e5f43250651e332600b3606d4d52ae65eb049d6c
zsalzwedel <zsalzwedel@yahoo.com>
2016-04-19 01:38:25 -0700

    made form look like kates gui

--- src/UserCreation/UserAccountSetup.java
+++ src/UserCreation/UserAccountSetup.java
@@ -7,9 +7,13 @@ import java.awt.*;
  * Created by Legoman60 on 4/19/2016.
  */
 public class UserAccountSetup {
-    private JTextField textField1;
-    private JButton clickButton;
+    private JTextField passField2;
+    private JButton finishButton;
     private JPanel rootPanel;
+    private JTextField passField1;
+    private JTextField nameField;
+    private JTextField emailField;
+    private JButton cancelButton;
 
     public UserAccountSetup() {
         rootPanel.setPreferredSize(new Dimension(500,350));
