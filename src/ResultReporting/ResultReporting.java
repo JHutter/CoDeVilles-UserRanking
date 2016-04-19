aaf298397abe7bad05dfd7b0657b66c382b7acae
zsalzwedel <zsalzwedel@yahoo.com>
2016-04-19 03:41:44 -0700

    name box and results box bot auto-populate with values from array lists

--- src/ResultReporting/ResultReporting.java
+++ src/ResultReporting/ResultReporting.java
@@ -14,8 +14,7 @@ public class ResultReporting {
     private JComboBox nameBox;
     private JPanel rootPanel;
     private JButton finishButton;
-    private JList resultList;
-
+    private JTextArea resultList;
     private ArrayList<UserAccount> userAccounts;
     private ArrayList<TestResult> testResults;
 
@@ -23,12 +22,62 @@ public class ResultReporting {
      * Default Constructor
      */
     public ResultReporting() {
-        //Set the size of the panel to agreed value
-        rootPanel.setPreferredSize(new Dimension(500, 350));
+        //Set the size of the panel to necessary value
+        rootPanel.setPreferredSize(new Dimension(560, 375));
+
+        //populate the test result list and user account list
+        userAccounts = new ArrayList<>();
+        getUserAccounts();
+        testResults = new ArrayList<>();
+        getTestResults();
+
+        //fill the text fields
+        fillNameBox();
+        fillResultsBox();
     }
 
     /**
      * @return rootPanel the root panel holding all other content
      */
     public JPanel getRootPanel(){return rootPanel;}
+
+    /**
+     * fills the user account array list with data
+     */
+    public void getUserAccounts(){
+        //populate with test values for now
+        userAccounts.add(new UserAccount(1, "lol@lol.com", "Zack", "pass"));
+        userAccounts.add(new UserAccount(2, "lol2@lol2.com", "Zack2", "pass2"));
+    }
+
+    /**
+     * fills the test result array list with data
+     */
+    public void getTestResults(){
+        //populate with test values for now
+        testResults.add(new TestResult(1, 1, 1, 1, 1));
+        testResults.add(new TestResult(2, 2, 2, 2, 2));
+    }
+
+    /**
+     * populates the name box with the usernames in the user account array list
+     */
+    public void fillNameBox(){
+        for(UserAccount userAccount : userAccounts) {
+            nameBox.addItem(userAccount.getName());
+        }
+    }
+
+    /**
+     * populates the results box with the values in the test results array list
+     */
+    public void fillResultsBox(){
+        resultList.setText("");
+        resultList.append("ResultID \t Question# \t ItemID \t SessionID \t Result \n");
+        for(TestResult testResult : testResults){
+            resultList.append(testResult.getResultID() + "\t" + testResult.getQuestionNumber() + "\t" + testResult.getItemID() + "\t" + testResult.getSessionID() + "\t" + testResult.getResult() + "\n");
+        }
+    }
+
+
 }
