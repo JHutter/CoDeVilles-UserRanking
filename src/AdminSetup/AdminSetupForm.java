0dd45bfe9b67cffe5fdb059413646b09b10817bb
kate-lee <jinsook.lee@pcc.edu>
2016-04-19 22:19:58 -0700

    Add MainAdminSetup.java

--- src/AdminSetup/AdminSetupForm.java
+++ src/AdminSetup/AdminSetupForm.java
@@ -2,25 +2,64 @@ package AdminSetup;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
- * Created by Chulwoo on 4/18/2016.
+ * The adminSetupForm class is made to control admin setup GUI screen.
+ * It add/delete items for test from user and update database.
+ * Items should be more than two. After any user takes test, item can not be changed.
+ * @author   Jinsook Lee
+ * @version  04/18/2016
  */
+
 public class AdminSetupForm {
-    private JTextField textField1;
-    private JPanel panel11;
-    private JList list1;
+    private JTextField itemTextField;
+    private JPanel rootPanel;
+    private JList itemList;
     private JButton addButton;
     private JButton deleteButton;
     private JButton finishButton;
     private JButton cancelButton;
+    private JLabel takenLabel;
+    private boolean isMoreThanTwo = false;
+    private boolean isTestTaken = false;
 
     public AdminSetupForm(){
-        panel11.setPreferredSize(new Dimension(500,300));
+        rootPanel.setPreferredSize(new Dimension(500,350));
+        takenLabel.setVisible(false);
+        addButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(rootPanel, "Item is added successfully.");
+            }
+        });
+        if (!isMoreThanTwo)     //If there are less than two items , finish button should be disabled
+        {
+            finishButton.setEnabled(false);
+        }
+        if (isTestTaken)        //After any user takes test, item can not be changed
+        {
+            addButton.setEnabled(false);
+            deleteButton.setEnabled(false);
+            takenLabel.setVisible(true);
+        }
+        deleteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(rootPanel, "Item is deleted successfully.");
+            }
+        });
+        cancelButton.addActionListener(new ActionListener() {       //Cancel button clears the item input text field
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                itemTextField.setText("");
+            }
+        });
     }
 
     public JPanel getRootPanel(){
-        return panel11;
+        return rootPanel;
 
     }
 }
