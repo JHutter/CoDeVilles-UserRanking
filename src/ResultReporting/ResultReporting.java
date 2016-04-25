05f0fa5bd8bac9b2bcf9822d93c45cf7d8472d6f
zsalzwedel <zsalzwedel@yahoo.com>
2016-04-24 20:19:37 -0700

    finished first pass on results reporting

--- src/ResultReporting/ResultReporting.java
+++ src/ResultReporting/ResultReporting.java
@@ -1,9 +1,9 @@
 package ResultReporting;
 
+import ContainerClasses.TestItem;
 import ContainerClasses.TestResult;
 import ContainerClasses.UserAccount;
 import SharedFunctions.DatabaseManager;
-
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -12,9 +12,9 @@ import java.util.ArrayList;
  * This is the form class for result reporting.
  * This form presents the results of tests to the user
  * CIS 234A Dougherty
- *
- *  Programmer(s): Zack
- *  Date: 4/19/2016.
+ * Date created: 4/19/2016.
+ *  @author Zack
+ *  @version 2016.24.4
  *
  */
 public class ResultReporting {
@@ -23,6 +23,7 @@ public class ResultReporting {
     private JButton finishButton;
     private JTextArea resultList;
     private ArrayList<UserAccount> userAccounts;
+    private ArrayList<TestItem> testItems;
     private ArrayList<TestResult> testResults;
     private DatabaseManager databaseManager;
 
@@ -35,9 +36,11 @@ public class ResultReporting {
         //instance the database manager
         databaseManager = new DatabaseManager();
 
-        //populate the test result list and user account list
+        //populate the test result list, test item list, and user account list
         userAccounts = new ArrayList<>();
         getUserAccounts();
+        testItems = new ArrayList<>();
+        getTestItems();
         testResults = new ArrayList<>();
         getTestResults();
 
@@ -62,9 +65,20 @@ public class ResultReporting {
     }
 
     /**
+     * fills the user account array list with data
+     */
+    public void getTestItems(){
+        //populate with values from the database
+        if(!databaseManager.readAllTestItems(testItems)){
+            //close the window due to read failure
+        }
+    }
+
+    /**
      * fills the test result array list with data
      */
     public void getTestResults(){
+        //populate with values from the database
         if(!databaseManager.readAllTestResults(testResults)){
             //close the window due to read failure
         }
@@ -80,15 +94,42 @@ public class ResultReporting {
     }
 
     /**
-     * populates the results box with the values in the test results array list
+     * populates the results box with values
+     * item names are from the test items array list
+     * results are parsed from the test results array list
      */
     public void fillResultsBox(){
+        int win = 0, loss = 0, tie = 0;
+
+        //reset the text box and add the header
         resultList.setText("");
-        resultList.append("ResultID \t Question# \t ItemID \t SessionID \t Result \n");
-        for(TestResult testResult : testResults){
-            resultList.append(testResult.getResultID() + "\t" + testResult.getQuestionNumber() + "\t" + testResult.getItemID() + "\t" + testResult.getSessionID() + "\t" + testResult.getResult() + "\n");
+        resultList.append("Item \t Win \t Loss \t Tie \n");
+
+        for(TestItem testItem : testItems){
+
+            //parse results
+            for(TestResult testResult : testResults){
+                //if the test item and the test result have the same item id, then count the result
+                if(testItem.getItemID()== testResult.getItemID()) {
+                    switch (testResult.getResult()) {
+                        case 1:
+                            win++;
+                            break;
+                        case 0:
+                            tie++;
+                            break;
+                        case -1:
+                            loss++;
+                            break;
+                    }
+                }
+            }
+            //write the data after parsing
+            resultList.append(testItem.getItemText() + "\t" + win + "\t" + loss + "\t" + tie + "\n");
+            //reset counters
+            win = 0;
+            loss = 0;
+            tie = 0;
         }
     }
-
-
 }
