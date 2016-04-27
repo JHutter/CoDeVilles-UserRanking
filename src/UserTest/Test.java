e62777e61c5f134578e6772fda80d915afe60510
JHutter <joanne.hutter@pcc.edu>
2016-04-26 23:32:34 -0700

    updating listeners for UserTest

--- src/UserTest/Test.java
+++ src/UserTest/Test.java
@@ -9,8 +9,9 @@ import java.util.Collections;
  */
 public class Test {
     /* fields */
-    private int turnCount;
-    private int itemCount;
+    private int turnTotal;
+    private int currentTurn;
+    private int itemTotal;
     private int testID;
     private ArrayList<String> items;
     private ArrayList<ItemPair> pairs;
@@ -18,17 +19,18 @@ public class Test {
 
     /* constructor */
     public Test() {
+        currentTurn = 0;
         items = getDBitems(testID);
+        itemTotal = items.size();
         pairs = generateItemPairs(items);
-        itemCount = items.size();
-        turnCount = pairs.size();
+        turnTotal = pairs.size();
     }
 
     private ArrayList<ItemPair> generateItemPairs(ArrayList<String> items) {
         ArrayList<ItemPair> generatedPairs = new ArrayList<ItemPair>();
-        for (int outer = 0; outer < itemCount-1; outer++) {
+        for (int outer = 0; outer < itemTotal-1; outer++) {
             String outerItem = items.get(outer);
-            for (int inner = outer+1; inner < itemCount; inner++) {
+            for (int inner = outer+1; inner < itemTotal; inner++) {
                 String innerItem = items.get(inner);
                 ItemPair currentPair = new ItemPair(outerItem, innerItem);
                 generatedPairs.add(currentPair);
@@ -44,4 +46,14 @@ public class Test {
         return new ArrayList<>(Arrays.asList("cat", "dog", "apple", "existential dread", "sunflower"));
     }
 
+    public void incrementTurn() {
+        currentTurn++;
+    }
+
+    public int getCurrentTurn() {
+        return currentTurn;
+    }
+    public int getTotalTurn() {
+        return turnTotal;
+    }
 }
--- src/UserTest/UserTakingTestForm.java
+++ src/UserTest/UserTakingTestForm.java
@@ -19,40 +19,58 @@ public class UserTakingTestForm {
     private JButton itemAButton;
     private JButton itemBButton;
     private JButton noItemButton;
-    private JButton nextButton;
     private JButton finishButton;
+    private Test test = new Test();
+    private JButton selectedItem;
 
     public UserTakingTestForm() {
         rootPanel.setPreferredSize(new Dimension(500,350));
         itemAButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.print("You clicked Item A!\n");
+                toggleItemButtonColor(itemAButton, itemBButton, noItemButton);
+                setSelectedItem(itemAButton);
             }
         });
         itemBButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.print("You clicked Item B!\n");
+                toggleItemButtonColor(itemBButton, itemAButton, noItemButton);
+                setSelectedItem(itemBButton);
             }
         });
         noItemButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.print("You clicked \"Can't Decide\"!\n");
+                toggleItemButtonColor(noItemButton, itemBButton, itemAButton);
+                setSelectedItem(noItemButton);
             }
         });
 
         finishButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.out.print("You clicked Finish!\n");
+                test.incrementTurn();
+                if (test.getCurrentTurn() == test.getTotalTurn()) {
+                    finishButton.setText("Finish");
+                }
+                System.out.print("You clicked Next/Finish!\nTurn: " + test.getCurrentTurn() + "\n");
             }
         });
     }
 
     public JPanel getRootPanel() {
         return rootPanel;
+    }
+
+    private void toggleItemButtonColor(JButton buttonClicked, JButton buttonOther, JButton buttonOtherOther) {
+        /* TODO make the colors prettier */
+        buttonClicked.setBackground(Color.lightGray);
+        buttonOther.setBackground(new Color(232,232,232));
+        buttonOtherOther.setBackground(new Color(232,232,232));
+    }
+    private void setSelectedItem(JButton button) {
+        selectedItem = button;
     }
 }
 
