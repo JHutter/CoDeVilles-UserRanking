dc2770ada03385256058bdaa6a5431f8f6022859
zsalzwedel <zsalzwedel@yahoo.com>
2016-04-27 22:37:53 -0700

    added classes for driver functionality that I previously worked on. Also resized the results reporting window

---
+++ src/SharedFunctions/JavaProcess.java
@@ -0,0 +1,47 @@
+package SharedFunctions;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * This class allows a java class to be run as a separate process by supplying a class containing main(String args[]){} as a parameter
+ *
+ * Credit to the user hallidave of stackoverflow.com for this code
+ * http://stackoverflow.com/questions/636367/executing-a-java-application-in-a-separate-process/723914#723914
+ *
+ * class creation date: 4/27/2016
+ *
+ * @author hallidave, Zack
+ * @version 2016.4.27
+ */
+public class JavaProcess {
+
+    /**
+     * default constructor is empty
+     */
+    private JavaProcess() {}
+
+    /**
+     * This function takes the supplied class and runs it as a new process and wait for it to finish
+     * @param klass The class that will be run in the new process
+     * @return exitValue The exit value the newly made process
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static int exec(Class klass) throws IOException,
+            InterruptedException {
+        String javaHome = System.getProperty("java.home");
+        String javaBin = javaHome +
+                File.separator + "bin" +
+                File.separator + "java";
+        String classpath = System.getProperty("java.class.path");
+        String className = klass.getCanonicalName();
+
+        ProcessBuilder builder = new ProcessBuilder(
+                javaBin, "-cp", classpath, className);
+
+        Process process = builder.start();
+        process.waitFor();
+        return process.exitValue();
+    }
+}
---
+++ src/SharedFunctions/Menu.java
@@ -0,0 +1,117 @@
+package SharedFunctions;
+
+import ResultReporting.TestResultReporting;
+import AdminSetup.MainAdminSetup;
+import UserCreation.TestUserCreation;
+import UserTest.TestUserTest;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+/**
+ * This class models the Main Menu for the project.
+ * It is a simple series of buttons
+ * When clicked it opens a new window of the respective form
+ * The new form is run in a new process
+ * Using System.exit() in the new form returns to the main menu
+ *
+ * Creation Date: 4/27/2016
+ *
+ * @author Zack
+ * @version 2016.4.27
+ */
+public class Menu {
+    int choice = 0; //The last item selected
+    private JButton adminSetupButton;
+    private JButton userSetupButton;
+    private JButton takeTestButton;
+    private JButton resultsButton;
+    private JPanel rootPanel;
+    private JButton exitButton;
+
+    public Menu() {
+
+        //Set the size of the panel to standard value
+        rootPanel.setPreferredSize(new Dimension(500, 350));
+
+        //listener for button that starts Admin Setup
+        adminSetupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 1;
+                try {
+                    int status = JavaProcess.exec(MainAdminSetup.class);
+                    JOptionPane.showMessageDialog(null, "Exit status: " + status);
+                } catch (IOException err) {
+                    err.printStackTrace();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+
+        //listener for button that starts User Account Creation
+        userSetupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 2;
+                try {
+                    int status = JavaProcess.exec(TestUserCreation.class);
+                    JOptionPane.showMessageDialog(null, "Exit status: " + status);
+                } catch (IOException err) {
+                    err.printStackTrace();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+
+        //listener for button that starts Test Taking
+        takeTestButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 3;
+                try {
+                    int status = JavaProcess.exec(TestUserTest.class);
+                    JOptionPane.showMessageDialog(null, "Exit status: " + status);
+                } catch (IOException err) {
+                    err.printStackTrace();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+
+        //listener for button that starts Result Reporting
+        resultsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                choice = 4;
+                try {
+                    int status = JavaProcess.exec(TestResultReporting.class);
+                    JOptionPane.showMessageDialog(null, "Exit status: " + status);
+                } catch (IOException err) {
+                    err.printStackTrace();
+                } catch (InterruptedException err) {
+                    err.printStackTrace();
+                }
+            }
+        });
+
+        //listener for exit button
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    /**
+     * @return rootPanel the root panel holding all other content
+     */
+    public JPanel getRootPanel(){return rootPanel;}
+}
