b4194d38d1e5b4b0ea45d07b7454a8d4d5292fe6
zsalzwedel <zsalzwedel@yahoo.com>
2016-04-29 16:08:34 -0700

    reduced number of pop-ups generated by account creation

--- src/UserCreation/UserAccountSetup.java
+++ src/UserCreation/UserAccountSetup.java
@@ -57,24 +57,31 @@ public class UserAccountSetup {
                         if (!passField1.getText().equals("") && passField1.getText().equals(passField2.getText())) {
 
                             //all tests passed, all fields have valid values. Create account object.
-                            JOptionPane.showMessageDialog(rootPanel, "Attempting to create account for " + nameField.getText() + "\n email: " + emailField.getText() + "\n password: " + passField1.getText());
                             account = new UserAccount(emailField.getText(), nameField.getText(), passField1.getText());
 
                             //call database manager and attempt to insert account. If email is not unique then database will error.
                             if (databaseManager.insertUserAccount(account)){
-                                JOptionPane.showMessageDialog(rootPanel, "Created account for " + account.getName() + "\n email: " + account.getEmail() + "\n password: " + account.getPassword());
+                                //show confirmation message if insert succeeds
+                                JOptionPane.showMessageDialog(rootPanel, "Created account for " + account.getName() +
+                                                                         "\nemail: " + account.getEmail() +
+                                                                         "\npassword: " + account.getPassword());
+                            } else {
+                                //show error message if insert fails
+                                JOptionPane.showMessageDialog(rootPanel, "Unable to create account. Please ensure you are using a unique email\n" +
+                                                                         "and check your internet connection");
                             }
                         } else {
                             //show message on invalid password
-                            JOptionPane.showMessageDialog(rootPanel, "Invalid Password. Both passwords must be the same (and not blank).");
+                            passField1.setText("Invalid Password.");
+                            passField2.setText("Both passwords must be the same (and not blank).");
                         }
                     } else {
                         //show message on blank name
-                        JOptionPane.showMessageDialog(rootPanel, "Name can not be blank.");
+                        nameField.setText("Name can not be blank.");
                     }
                 } else {
                     //show message on blank email
-                    JOptionPane.showMessageDialog(rootPanel, "Email can not be blank.");
+                    emailField.setText("Email can not be blank.");
                 }
             }
         });
