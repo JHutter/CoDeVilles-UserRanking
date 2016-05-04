f67160cdc90857f69aec4d78f10bd02786c0d8e7
J Hutter <jeh.hutter@gmail.com>
2016-05-03 23:47:43 -0700

    sign in form

---
+++ src/UserTest/UserSignInForm.java
@@ -0,0 +1,15 @@
+package UserTest;
+
+import javax.swing.*;
+
+/**
+ * Created by JoAnne on 5/3/2016.
+ */
+public class UserSignInForm {
+    private JPanel panel1;
+    private JPasswordField emailPasswordField;
+    private JButton button1;
+    private JComboBox comboBox1;
+    private JTextArea welcomePleaseSignInTextArea;
+    private JTextField emailTextField;
+}
