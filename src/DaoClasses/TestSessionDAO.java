522ab0d16edfb6c65176fc0df6d8a2aa91b12b04
J Hutter <jeh.hutter@gmail.com>
2016-05-24 16:54:53 -0700

    added TestSessionDAO and implementation

--- src/DaoClasses/TestSessionDAO.java
+++ src/DaoClasses/TestSessionDAO.java
@@ -1,7 +1,19 @@
 package DaoClasses;
 
+import ContainerClasses.TestSession;
+
+import java.util.ArrayList;
+
 /**
- * Created by JoAnne on 5/24/2016.
+ * This class lists all methods in the TestResultDAOimpl class
+ * CIS 234A Dougherty
+ * Date created: 5/24/2016.
+ *  @author JoAnne Hutter
+ *  @version 2016.5.24
  */
-public class TestSessionDAO {
+public interface TestSessionDAO {
+    public void insertSession(int userID, int testID);
+    public boolean readAllTestSessions(ArrayList<TestSession> testSessions);
+    public int getSessionID(int userID, int testID);
+
 }
--- src/DaoClasses/TestSessionDAOimpl.java
+++ src/DaoClasses/TestSessionDAOimpl.java
@@ -1,7 +1,142 @@
 package DaoClasses;
 
+import ContainerClasses.TestSession;
+import DaoClasses.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
- * Created by JoAnne on 5/24/2016.
+ * This class implements the methods in the TestSessionDAO class
+ *
+ * Date created: 05/24/2016
+ *  @author JoAnne Hutter
+ *  @version 05/24/2016
  */
-public class TestSessionDAOimpl {
+public class TestSessionDAOimpl implements TestSessionDAO {
+    //fields
+    DatabaseConnection databaseConnection;
+    private static final String GET_SESSION_SQL = "SELECT SessionID FROM TESTSESSIONS WHERE UserID = ? and TestID = ?";
+    private static final String INSERT_NEW_SESSION_SQL = "INSERT INTO TESTSESSIONS (UserID, TestID, isActive) values (?, ?, 0)";
+    private static final String GET_ALL_TEST_SESSIONS_SQL = "SELECT SessionID, UserID, TestID, isActive FROM TESTSESSIONS";
+    private static final String SET_IS_ACTIVE_SQL = "UPDATE TESTSESSIONS SET isActive = 1 WHERE SessionID = ?" +
+            "and UserID = ? and  TestID = ?";
+
+    //constructor
+    public TestSessionDAOimpl(){
+        databaseConnection = new DatabaseConnection();
+    }
+
+    //methods
+    /**
+     *  Insert a new test session in the database
+     * @param  userID the ID of the user taking the test
+     * @param  testID the ID of the test being taken
+     */
+    public void insertSession(int userID, int testID){
+        try ( //try to create a database connection
+              Connection connection =  databaseConnection.getConnection();
+              PreparedStatement stmt = connection.prepareStatement(INSERT_NEW_SESSION_SQL);
+        ){
+            stmt.setInt(1, userID);
+            stmt.setInt(2, testID);
+            stmt.executeUpdate();
+            return;
+        }
+        catch (SQLException e) { //if the connection fails, show error
+            System.err.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    /**
+     * Reads all test sessions from the database and informs caller of success with return boolean
+     * @param testSessions An ArrayList of TestSession to populate with data from the database
+     * @return true/false Whether the connection and read failed or succeeded.
+     */
+    public boolean readAllTestSessions(ArrayList<TestSession> testSessions) {
+        boolean isSessionActive;
+        try ( //try to create a database connection
+              Connection connection = databaseConnection.getConnection();
+              PreparedStatement stmt = connection.prepareStatement(GET_ALL_TEST_SESSIONS_SQL);
+              ResultSet rs = stmt.executeQuery()
+        ) {
+            while (rs.next()) { //if the connection was successful, read the result set and put into array list
+                if (rs.getInt("isActive") == 1) {
+                    isSessionActive = true;
+                } else {
+                    isSessionActive = false;
+                }
+                testSessions.add(
+                        new TestSession(
+                                rs.getInt("SessionID"),
+                                rs.getInt("TestID"),
+                                rs.getInt("UserID"),
+                                isSessionActive
+                        ));
+            }
+
+        } catch (SQLException e) { //if the connection fails, show error
+            System.err.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+            return false; //return false due to connection failure
+        }
+        return true;
+    }
+
+    /**
+     *  Get a test session from the database
+     * @param  userID the ID of the user taking the test
+     * @param  testID the ID of the test being taken
+     * @return sessionID the ID of the testSession
+     */
+    public int getSessionID(int userID, int testID) {
+        try ( //try to create a database connection
+              Connection connection =  databaseConnection.getConnection();
+              PreparedStatement stmt = connection.prepareStatement(GET_SESSION_SQL);
+        ){
+            stmt.setInt(1, userID);
+            stmt.setInt(2, testID);
+            ResultSet rs = stmt.executeQuery();
+            int sessionID = 3; // TODO troubleshoot this loop
+            while (rs.next()) {
+                sessionID = rs.getInt("SessionID");
+            }
+            return sessionID;
+        }
+        catch (SQLException e) { //if the connection fails, show error
+            System.err.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /**
+     * Set isActive to true when a test is being taken by the user
+     * @param sessionID int, the ID for the session
+     * @param userID int, the ID for the user
+     * @param testID int, the ID for the test
+     */
+    public void setIsActive(int sessionID, int userID, int testID) {
+        try ( //try t create a database connection
+              Connection connection =  databaseConnection.getConnection();
+              PreparedStatement stmt = connection.prepareStatement(SET_IS_ACTIVE_SQL);
+        ){
+            stmt.setInt(1, sessionID);
+            stmt.setInt(2, userID);
+            stmt.setInt(3, testID);
+            stmt.executeUpdate();
+            return;
+        }
+        catch (SQLException e) { //if the connection fails, show error
+            System.err.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+    }
+
+
 }
